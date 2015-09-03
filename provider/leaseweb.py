3f82f4a2161e7026bdc42935ac205bc2541cdd85
IOjeffrey <6321936+IOjeffrey@users.noreply.github.com>
2015-09-03 22:32:18 +0200

    GET and PUT for LeaseWeb working

--- provider/leaseweb.py
+++ provider/leaseweb.py
@@ -7,6 +7,9 @@ from conf import LeasewebApiKey
 # Request: LeaseWeb API (https://api.leaseweb.com/v1/bareMetals)
 connection = httplib.HTTPSConnection('api.leaseweb.com')
 
+#Set debug output bool
+connection.set_debuglevel(1)
+
 # Headers
 headers = LeasewebApiKey
 
@@ -17,9 +20,132 @@ try:
     #TODO: check if result is json before doing this
     bareMetal = json.loads(response.read())
     # Success
-    print('Response status ' + str(response.status))
+    print('Response status and reason ' + str(response.status) + ' ' + str(response.reason))
 except httplib.HTTPException, e:
     # Exception
     print('Exception during request')
 
-print bareMetal
+#Example output JSON
+#print json.dumps(bareMetal, sort_keys=True, indent=4)
+#    "bareMetals": [
+#        {
+#            "bareMetal": {
+#                "bareMetalId": "195591",   #JWZF010 = 195604
+#                "reference": null,
+#                "serverName": "JWZF001",
+#                "serverType": "Bare Metal"
+#            }
+#        },
+for jef in bareMetal['bareMetals']:
+    print jef['bareMetal']['serverName']
+
+connection.request('GET', '/v1/bareMetals/195591/rootPassword.json', None, headers)
+try:
+    response = connection.getresponse()
+    #TODO: check if result is json before doing this
+    rootPass = json.loads(response.read())
+    # Success
+    print('Response status and reason ' + str(response.status) + ' ' + str(response.reason))
+except httplib.HTTPException, e:
+    # Exception
+    print('Exception during request')
+
+print rootPass['rootPassword']
+
+connection.request('GET', '/v1/bareMetals/195604/installationStatus', None, headers)
+try:
+    response = connection.getresponse()
+    #TODO: check if result is json before doing this
+    osstatus = json.loads(response.read())
+    # Success
+    print('Response status and reason ' + str(response.status) + ' ' + str(response.reason))
+except httplib.HTTPException, e:
+    # Exception
+    print('Exception during request')
+
+print osstatus
+
+connection.request('GET', '/v1/bareMetals/195604/switchPort', None, headers) #add /open or /close to enable or disable port
+try:
+    response = connection.getresponse()
+    #TODO: check if result is json before doing this
+    switch = json.loads(response.read())
+    # Success
+    print('Response status and reason ' + str(response.status) + ' ' + str(response.reason))
+except httplib.HTTPException, e:
+    # Exception
+    print('Exception during request')
+
+print switch
+
+connection.request('GET', '/v1/bareMetals/195604/powerStatus', None, headers) #add /open or /close to enable or disable port
+try:
+    response = connection.getresponse()
+    #TODO: check if result is json before doing this
+    powerstatus = json.loads(response.read())
+    # Success
+    print('Response status and reason ' + str(response.status) + ' ' + str(response.reason))
+except httplib.HTTPException, e:
+    # Exception
+    print('Exception during request')
+
+print powerstatus
+
+connection.request('GET', '/v1/bareMetals/195604/ips', None, headers) #add /open or /close to enable or disable port
+try:
+    response = connection.getresponse()
+    #TODO: check if result is json before doing this
+    ip = json.loads(response.read())
+    # Success
+    print('Response status and reason ' + str(response.status) + ' ' + str(response.reason))
+except httplib.HTTPException, e:
+    # Exception
+    print('Exception during request')
+
+print ip
+
+
+#Create postheader and combine it with the normal header
+postHeader = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+postHeader.update(headers)
+
+connection.request('PUT', '/v1/bareMetals/195604/ips/85.17.25.77', "reverseLookup=my.domain.nl&nullRouted=0", postHeader) #add /open or /close to enable or disable port
+try:
+    response = connection.getresponse()
+    #TODO: check if result is json before doing this
+    ipUpdate = json.loads(response.read())
+    # Success
+    print('Response status and reason ' + str(response.status) + ' ' + str(response.reason))
+except httplib.HTTPException, e:
+    # Exception
+    print('Exception during request')
+
+print ipUpdate
+
+connection.request('GET', '/v1/bareMetals/195604?billing=1&details=1', None, headers) #add /open or /close to enable or disable port
+try:
+    response = connection.getresponse()
+    #TODO: check if result is json before doing this
+    details2 = json.loads(response.read())
+    # Success
+    print('Response status and reason ' + str(response.status) + ' ' + str(response.reason))
+except httplib.HTTPException, e:
+    # Exception
+    print('Exception during request')
+
+print details2
+
+connection.request('PUT', '/v1/bareMetals/195604', "reference=name", postHeader) #add /open or /close to enable or disable port
+try:
+    response = connection.getresponse()
+    #TODO: check if result is json before doing this
+    detailsUpdate = json.loads(response.read())
+    # Success
+    print('Response status and reason ' + str(response.status) + ' ' + str(response.reason))
+except httplib.HTTPException, e:
+    # Exception
+    print('Exception during request')
+
+print detailsUpdate
+
+connection.close()
